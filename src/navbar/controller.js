c0eaf6b59d8bb717b6c10c15b6f081cecf1b0709
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-11-08 16:07:20 +0200

    remove board on logout

--- src/navbar/controller.js
+++ src/navbar/controller.js
@@ -1,13 +1,16 @@
 export default class NavbarController {
-  constructor(taskService) {
+  constructor(taskService, $timeout) {
     'ngInject';
+    this.$timeout = $timeout;
     this.taskService = taskService;
     this.showBoards = false;
     this.showLogin = false;
   }
 
   logout() {
-    this.taskService.user = {};
+      this.taskService.user = {};
+      this.taskService.board.name = undefined; // [Evgeniy Tatatrin - 11/08/2016] hide board template as far it looks at board's name exist
+      this.taskService.board = {};
   }
 
 }
